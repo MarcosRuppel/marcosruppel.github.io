2d154adfda8c59d16cba00358f6056ce0e21989c
Marcos Paulo Ruppel <marcospr@outlook.com>
2023-06-02 16:59:28 -0300

    Primeiro upload

---
+++ mySlides.js
@@ -0,0 +1,31 @@
+// Script JS para exibição de carrossel de imagens com controles interativos
+
+// Slide em exibição
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Controles anterior/próximo
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Controles miniaturas
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
